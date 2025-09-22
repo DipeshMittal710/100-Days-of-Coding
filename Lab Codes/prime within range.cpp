6a39f763c7866755aea9b4da5ae487afe95b0e00
Dipesh Mittal <dipeshmittal71006@gmail.com>
2025-09-22 19:05:19 +0530

    Add files via upload

---
+++ Lab Codes/prime within range.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int lower, upper, i, j, isPrime;
+    printf("Enter lower and upper range: ");
+    scanf("%d %d", &lower, &upper);
+
+    printf("Prime numbers between %d and %d are:\n", lower, upper);
+    for (i = lower; i <= upper; i++) {
+        if (i < 2) continue; 
+        isPrime = 1; 
+        for (j = 2; j <= i/2; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if (isPrime)
+            printf("%d ", i);
+    }
+    printf("\n");
+    return 0;
+}
+
